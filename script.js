e3faa4d00ce99e1c82970b1fcf65395ebec85f79
ipeng5 <ypeng115@gmail.com>
2022-08-20 19:32:28 +0200

    Build the canvas and bacic functions (colors and grid size)

--- script.js
+++ script.js
@@ -3,17 +3,33 @@ const range = document.getElementById("range");
 const penColor = document.getElementById("pen-color");
 const bgColor = document.getElementById("background-color");
 const eraser = document.getElementById("eraser");
-const gridLine = document.getElementById("grid-line");
-let trigger = false;
+const random = document.getElementById("random");
+const gridLinesSwitch = document.getElementById("grid-lines");
+const reset = document.getElementById("reset");
+const cells = document.querySelectorAll(".cell");
+let gridLinesOn = true;
+let drawing = false;
+let randomOn = false;
 let maxRow = 30;
+let ranColor
 
-container.addEventListener('mousedown', () => {
-    trigger = true;
+container.addEventListener('mousedown', () => { drawing = true });
+container.addEventListener('mouseup', () => { drawing = false });
+
+
+// Reset the board
+reset.addEventListener('click', () => {
+    let cells = document.querySelectorAll(".cell");
+    maxRow = 30;
+    document.getElementById("result").textContent = "30 x 30"
+    range.value = 30;
+    createGrid(30);
+    container.style.backgroundColor = "#ffffff";
+    for (let cell of cells) { cell.style.backgroundColor = container.style.backgroundColor };
 });
 
-container.addEventListener('mouseup', () => {
-    trigger = false;
-});
+
+gridLinesSwitch.addEventListener("change", toggleGridLines)
 
 // create cells in grid
 function createGrid(maxRow, maxRow) {
@@ -23,34 +39,82 @@ function createGrid(maxRow, maxRow) {
         container.style.gridTemplateRows = `repeat(${maxRow}, 1fr)`;
         container.style.gridTemplateColumns = `repeat(${maxRow}, 1fr)`;
         cell.classList.add('cell');
-        cell.style.border = "1px solid gray";
-        cell.addEventListener('mousedown', () => { cell.style.backgroundColor = penColor.value });
-        cell.addEventListener('mouseover', () => { if (trigger) cell.style.backgroundColor = penColor.value });
+        cell.addEventListener('mouseover', () => {
+             ranColor = "white";
+
+            if (drawing && !randomOn) { cell.style.backgroundColor = penColor.value }
+            if (randomOn) { getRandom(); };
+            function getRandom() {
+                let r = Math.floor(Math.random() * 255);
+                let g = Math.floor(Math.random() * 255);
+                let b = Math.floor(Math.random() * 255);
+                color = `rgb(${r}, ${g}, ${b})`;
+                ranColor.value = rgbToHex(r, g, b);
+                cell.style.backgroundColor = ranColor.value;
+            }
+
+            function rgbToHex(r, g, b) {
+                return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+            };
+            // Convert RGB to HEX
+            function componentToHex(r) {
+                let hex = r.toString(16);
+                return hex.length == 1 ? "0" + hex : hex;
+            };
+        });
+        cell.addEventListener('mousedown', () => {
+            if (randomOn) {
+                if (randomOn) { getRandom(); };
+                function getRandom() {
+                    let r = Math.floor(Math.random() * 255);
+                    let g = Math.floor(Math.random() * 255);
+                    let b = Math.floor(Math.random() * 255);
+                    color = `rgb(${r}, ${g}, ${b})`;
+                    ranColor.value = rgbToHex(r, g, b);
+                    cell.style.backgroundColor = ranColor.value;
+                }
+
+                function rgbToHex(r, g, b) {
+                    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+                };
+                // Convert RGB to HEX
+                function componentToHex(r) {
+                    let hex = r.toString(16);
+                    return hex.length == 1 ? "0" + hex : hex;
+                };
+            } else { cell.style.backgroundColor = penColor.value };
+        });
         bgColor.addEventListener("change", () => { container.style.backgroundColor = bgColor.value });
+        if (gridLinesOn) { cell.classList.add("grid-lines") };
+
     }
 }
-createGrid(maxRow, maxRow)
+
+random.addEventListener("click", () => {
+    randomOn = !randomOn;
+    random.classList.toggle("active");
+    console.log(random.classList)
+})
 
 
-gridLine.addEventListener("change", changeGrid)
-function changeGrid() {
-     if (e.target.checked) { console.log("checked") } else { container.style.backgroundColor = "red" } 
+
+function toggleGridLines() {
+    let cells = document.querySelectorAll(".cell");
+    gridLinesOn = !gridLinesOn;
+    for (let cell of cells) { cell.classList.toggle("grid-lines") }
+    container.classList.toggle("grid-lines");
 }
 
+createGrid(maxRow, maxRow)
+rangeUpdate()
 
 // display current range value
 range.addEventListener("input", rangeUpdate)
-function rangeUpdate() {
-    document.getElementById("result").textContent = `${range.value} x ${range.value}`;
-}
-
-// display default size range value
-rangeUpdate()
+function rangeUpdate() { document.getElementById("result").textContent = `${range.value} x ${range.value}` }
 
 // update grid size based on slider value
 range.addEventListener("mouseup", updateGrid);
 function updateGrid() {
-    let cells = container.querySelectorAll(".cell");
     cells.forEach(cell => cell.remove());
     createGrid(range.value, range.value);
 }
